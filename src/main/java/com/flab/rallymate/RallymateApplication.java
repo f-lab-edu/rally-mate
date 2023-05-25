045c3add55408d0a1eeec4b2dae70e88fe144e2a
hjun-park <phj0860@gmail.com>
2023-05-26 07:56:51 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/flab/rallymate/RallymateApplication.java
@@ -0,0 +1,13 @@
+package com.flab.rallymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RallymateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/rallymate/RallymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.rallymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
