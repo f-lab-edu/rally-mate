8bf6f5b604a14925e2e10df5fbc94bc402546188
hjun-park <phj0860@gmail.com>
2023-06-08 16:14:31 +0900

    chore: jasypt AES 적용

--- src/main/java/com/flab/rallymate/common/config/JasyptConfig.java
+++ src/main/java/com/flab/rallymate/common/config/JasyptConfig.java
@@ -1,26 +1,35 @@
 package com.flab.rallymate.common.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableEncryptableProperties
 public class JasyptConfig {
 
     @Value("${jasypt.encryptor.password}")
     private String encryptKey;
 
+    @Value("${jasypt.encryptor.algorithm}")
+    private String algorithm;
+
 
     @Bean(name = "jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(encryptKey);
         config.setPoolSize("1");
-        config.setAlgorithm("PBEWithMD5AndDES");    // Password Based Encryption (MD5 + DES)
+        config.setAlgorithm(algorithm);
+        config.setProvider(new BouncyCastleProvider());
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
         config.setStringOutputType("base64");
         config.setKeyObtentionIterations("1000");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
--- src/test/java/com/flab/rallymate/common/config/JasyptConfigTest.java
+++ src/test/java/com/flab/rallymate/common/config/JasyptConfigTest.java
@@ -1,6 +1,8 @@
 package com.flab.rallymate.common.config;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,7 +16,18 @@ public class JasyptConfigTest extends JasyptConfig {
     @ValueSource(strings = {"a", "b"})
     public void 문자열을_jasypt로_암호화한다(String plainText) {
         StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
-        jasypt.setPassword("password");
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword("pass");
+        config.setPoolSize("1");
+        config.setAlgorithm("algo");
+        config.setProvider(new BouncyCastleProvider());
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        jasypt.setConfig(config);
 
         String encryptedText = jasypt.encrypt(plainText);
         String decryptedText = jasypt.decrypt(encryptedText);
