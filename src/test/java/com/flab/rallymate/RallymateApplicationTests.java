beb0f2dbfee52d0a9f61d7c6c11e1371b3d811d0
hjun-park <phj0860@gmail.com>
2023-06-08 17:14:10 +0900

    chore: 테스트 전용 yml 미존재에 따른 SpringBootTest 어노테이션 제거

--- src/test/java/com/flab/rallymate/RallymateApplicationTests.java
+++ src/test/java/com/flab/rallymate/RallymateApplicationTests.java
@@ -1,9 +1,8 @@
 package com.flab.rallymate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RallymateApplicationTests {
 
 	@Test
