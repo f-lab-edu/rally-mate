3c6c8bfe1d4dcaafe89add55219ea453970e2db3
hjun-park <phj0860@gmail.com>
2023-07-01 11:29:08 +0900

    fix: ErrorCode HttpStatus 변경 및 불필요한 ErrorCode 제거

--- src/main/java/com/flab/rallymate/error/ErrorCode.java
+++ src/main/java/com/flab/rallymate/error/ErrorCode.java
@@ -4,40 +4,24 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-	PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "password not match"),
-	INVALID_SOCIAL_TYPE(HttpStatus.BAD_REQUEST, "Cannot find social type"),
-	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "User email duplicated."),
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User is not found."),
-	EMPTY_USER(HttpStatus.NOT_FOUND, "User is empty."),
-	NOT_AUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "Not Authorized access"),
-	INVALID_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "Invalid Authentication"),
-	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token"),
-	INVALID_TOKEN_ALGORITHM(HttpStatus.UNAUTHORIZED, "Invalid Token Algorithm"),
-	NOT_FOUND_KAKAO_USER_INFO(HttpStatus.NOT_FOUND, "Not found Kakao user info"),
-	NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "Not found member"),
-	ACCESS_DENIED(HttpStatus.FORBIDDEN, "Access is denied"),
-	DUMMY(HttpStatus.CONFLICT, "");
+	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않는 토큰입니다"),
+	NOT_FOUND_MEMBER(HttpStatus.UNAUTHORIZED, "해당 유저를 찾을 수 없습니다"),
+	PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "패스워드가 일치하지 않습니다"),
+
+
+	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다"),
+	ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다");
 
 	private final HttpStatus httpStatus;
-	private final String result;
 	private final String message;
 
 	ErrorCode(HttpStatus httpStatus, String message) {
 		this.httpStatus = httpStatus;
-		if (httpStatus.is4xxClientError()) {
-			this.result = BaseHttpResponse.FAIL;
-		} else {
-			this.result = BaseHttpResponse.ERROR;
-		}
 		this.message = message;
 	}
 
 	public HttpStatus getHttpStatus() {
 		return httpStatus;
-	}
-
-	public String getStatus() {
-		return result;
 	}
 
 	public String getMessage() {
