db54e1b85ae7bf1a8045214f4d322bf457aa73b9
hjun-park <phj0860@gmail.com>
2023-07-31 16:27:43 +0900

    test: 메이트 구인 Controller Layer 테스트코드 추가작성

--- src/test/java/com/flab/rallymate/post/rest/PostControllerTest.java
+++ src/test/java/com/flab/rallymate/post/rest/PostControllerTest.java
@@ -18,7 +18,6 @@ import java.time.LocalDateTime;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,11 +49,97 @@ public class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(content))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message", Matchers.is("구인글을 등록했어요.")));
 
 
         verify(postService).addPost(postRequestDTO);
+    }
+
+    @Test
+    void addPost_구인글의_시작시간이_현재보다_과거면_BAD_REQUEST를_리턴한다() throws Exception {
+        var postRequestDTO = PostRequestDTO.builder()
+                .playgroundId(1L)
+                .playTime(30)
+                .startTime(LocalDateTime.now().minusHours(1L))
+                .build();
+        String content = objectMapper.writeValueAsString(postRequestDTO);
+
+
+        client.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", Matchers.is("현재 시간 이후로 입력해 주세요.")));
+    }
+
+    @Test
+    void addPost_구인글_등록시_플레이공간이_누락되었다면_등록에_실패한다() throws Exception {
+        var postRequestDTO = PostRequestDTO.builder()
+                .playTime(30)
+                .startTime(LocalDateTime.now().plusHours(1L))
+                .build();
+        String content = objectMapper.writeValueAsString(postRequestDTO);
+
+
+        client.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", Matchers.is("플레이 공간을 선택해 주세요.")));
+    }
+
+    @Test
+    void addPost_구인글_등록시_플레이타임이_누락되었다면_등록에_실패한다() throws Exception {
+        var postRequestDTO = PostRequestDTO.builder()
+                .playgroundId(1L)
+                .startTime(LocalDateTime.now().plusHours(1L))
+                .build();
+        String content = objectMapper.writeValueAsString(postRequestDTO);
+
+
+        client.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", Matchers.is("플레이 시간을 입력해 주세요.")));
+    }
+
+    @Test
+    void addPost_구인글_등록시_플레이타임이_음수값이라면_등록에_실패한다() throws Exception {
+        var postRequestDTO = PostRequestDTO.builder()
+                .playgroundId(1L)
+                .startTime(LocalDateTime.now().plusHours(1L))
+                .playTime(-1)
+                .build();
+        String content = objectMapper.writeValueAsString(postRequestDTO);
+
+
+        client.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", Matchers.is("플레이 시간은 0보다 큽니다.")));
+    }
+
+    @Test
+    void addPost_구인글_등록시_시작시간이_누락되었다면_등록에_실패한다() throws Exception {
+        var postRequestDTO = PostRequestDTO.builder()
+                .playgroundId(1L)
+                .playTime(10)
+                .build();
+        String content = objectMapper.writeValueAsString(postRequestDTO);
+
+
+        client.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", Matchers.is("시작 시간을 입력해 주세요.")));
     }
 }
