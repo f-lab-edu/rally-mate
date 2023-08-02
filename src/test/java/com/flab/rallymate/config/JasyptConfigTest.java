da750cbe4df207a2ceb1a2d4b3ad9a1db07ceda6
Hyunjun Park <70880695+hjun-park@users.noreply.github.com>
2023-08-02 16:33:01 +0900

    Chore/docker mysql (#5)

--- src/test/java/com/flab/rallymate/config/JasyptConfigTest.java
+++ src/test/java/com/flab/rallymate/config/JasyptConfigTest.java
@@ -1,7 +1,5 @@
 package com.flab.rallymate.config;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
@@ -9,20 +7,20 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import com.flab.rallymate.config.JasyptConfig;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class JasyptConfigTest extends JasyptConfig {
 
 	@Disabled
 	@ParameterizedTest
-	@ValueSource(strings = {"a", "b"})
+	@ValueSource(strings = {"1", "2"})
 	public void 문자열을_jasypt로_암호화한다(String plainText) {
 		StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
 		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
 
-		config.setPassword("pass");
+		config.setPassword("encPassword");	// SET
 		config.setPoolSize("1");
-		config.setAlgorithm("algo");
+		config.setAlgorithm("algorithm");	// SET
 		config.setProvider(new BouncyCastleProvider());
 		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
 		config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
