0e515b68f92b6a908a0b8e4a85bbf79413988320
hjun-park <phj0860@gmail.com>
2023-08-07 16:15:33 +0900

    Chore: Exception Handler에서 Bean Validation 처리하도록 변경

--- src/main/java/com/flab/rallymate/error/BaseExceptionHandler.java
+++ src/main/java/com/flab/rallymate/error/BaseExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.flab.rallymate.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,6 +21,12 @@ public class BaseExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(BaseHttpResponse.error(ex.getMessage()));
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseHttpResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var baseHttpResponse = e.getBindingResult().getAllErrors().stream().findFirst()
+                .map(objectError -> BaseHttpResponse.error(objectError.getDefaultMessage()))
+                .orElseGet(() -> BaseHttpResponse.error(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(baseHttpResponse);
+    }
 
 }
-
